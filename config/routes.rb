6b554a616b85a0f33ae2dea212098d2be01790ab
miharayukito <mihara2994@gmail.com>
2024-06-10 17:09:11 +0900

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get :top, to: "top#index"
   get :mypage, to: "mypage#index"
+  
+  resources :users, only: [:index, :show]
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
